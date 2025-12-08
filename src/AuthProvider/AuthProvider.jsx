c57250c41855e4d6eceb00703a91e0f137b7d5fd
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-12-09 01:13:56 +0600

    google login success

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import AuthContext from '../AuthContext/AuthContext'
+import { AuthContext } from '../AuthContext/AuthContext';
+import {  GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from '../../firebase';
 
 const AuthProvider = ({ children }) => {
+    const provider = new GoogleAuthProvider();
+
+    // google login
+    const GoogleSignIN = () => {
+        return (signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            console.log(user);
+
+        }).catch((error) => {
+            console.log(error)
+        }));
+    }
+
+
+
 
     const authInfo = {
-
+        GoogleSignIN
     }
     return (
         <AuthContext value={authInfo}>
